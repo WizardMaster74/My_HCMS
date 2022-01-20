b6f3b3baa443b81561b1718590b0e66f561c8dc3
Kapil <kapildtandel74@gmail.com>
2022-01-20 12:58:59 +0530

    My Meeting section for patients

---
+++ book_doctor/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('search_doc', views.search_doc, name = "search_doc"),
+    path('select_date', views.select_date, name = "select_date"),
+    path('select_slot', views.select_slot, name = "select_slot"),
+    path('vid_details', views.vid_details, name = "vid_details"),
+    path('vd_mpay', views.vd_mpay, name = "vd_mpay"),
+    path('my_meeting', views.my_meeting, name = "my_meeting"),
+]
+
